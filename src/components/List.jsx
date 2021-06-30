3182322bba13f6459866dc611008a1cd77d7a7c9
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-06-30 11:31:37 -0500

    Init collapsable List

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+import "../styles/List.css";
 
 const List = ({ values, name, sortFn }) => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleCollapsed = () => {
+    setCollapsed((prevCollapsed) => !prevCollapsed);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter" || e.key === " ") {
+      toggleCollapsed();
+    }
+  };
+
   const orderedValues = sortFn ? values.sort(sortFn) : values;
   return (
-    <div>
-      <h2>{name}</h2>
-      <ul>
+    <div className={`list${collapsed ? " list--collapsed" : ""}`}>
+      <h2
+        className="list__header"
+        tabIndex="0"
+        role="button"
+        aria-pressed="false"
+        onClick={toggleCollapsed}
+        onKeyPress={handleKeyPress}
+      >
+        {name}
+      </h2>
+      <ul className="list__items">
         {orderedValues.map(({ id, name }) => (
           <li key={id}>{name}</li>
         ))}
